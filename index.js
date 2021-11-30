689fd0b5347848476764b91f65f6ddbb7438ed25
Tenzin Jorden <tenden96@gmail.com>
2021-11-29 17:38:16 -0800

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(name = "Josh") {
+    return "Hi, my name is "+ name+".";
+}
+
+function introductionWithLanguage(name = "Josh", language = "Ember.js"){
+    return "Hi, my name is "+name+" and I am learning to program in "+language+".";
+}
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript"){
+    return "Hi, my name is "+name+" and I am learning to program in "+language+".";
+}
